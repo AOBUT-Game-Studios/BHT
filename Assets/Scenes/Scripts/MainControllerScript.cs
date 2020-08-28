7570b5320806d924ad574df351d08ab28130d9ac
Hamza Sar <hamoz.sq78@gmail.com>
2020-08-28 14:42:12 -0400

    mostly done stamina code

--- Assets/Scenes/Scripts/MainControllerScript.cs
+++ Assets/Scenes/Scripts/MainControllerScript.cs
@@ -7,6 +7,15 @@ public class MainControllerScript : MonoBehaviour
     // movement
     public float walkingSpeed;
     public float runningSpeed;
+    public bool enableStamina;
+    float stamina;
+    public float maxStamina;
+    public float staminaLoseMultiplier;
+    public float staminaGainMultiplier;
+    public int staminaSpeed;
+    int staminaTimeout;
+    bool staminaDisabled;
+    
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -16,6 +25,16 @@ public class MainControllerScript : MonoBehaviour
         // movement
         walkingSpeed = 2.0f;
         runningSpeed = 5.0f;
+
+        // stamina stuff
+        enableStamina = true;
+        maxStamina = 10.0f;
+        stamina = maxStamina;
+        staminaLoseMultiplier = 1.0f;
+        staminaGainMultiplier = 1.0f;
+        staminaSpeed = 1;
+        staminaTimeout = 1;
+        staminaDisabled = false;
         rb = GetComponent<Rigidbody2D>();
                 
     }
@@ -31,11 +50,13 @@ public class MainControllerScript : MonoBehaviour
     }
     void move()
     {
-        float horizontal, vertical;
+        // movement variables
+        float horizontal, vertical, speed;
         bool up = Input.GetKey(KeyCode.W);
         bool down = Input.GetKey(KeyCode.S);
         bool left = Input.GetKey(KeyCode.A);
         bool right = Input.GetKey(KeyCode.D);
+        // if pressing left and right at the same time
         if (right && left) 
         {
             horizontal = 0.0f;
@@ -52,6 +73,12 @@ public class MainControllerScript : MonoBehaviour
         {
             horizontal = 0.0f;
         }
+
+        // if pressing up and down at the same time
+        if(up && down) 
+        {
+            vertical = 0.0f;
+        }
         if(up)
         {
             vertical = 1.0f;
@@ -64,20 +91,66 @@ public class MainControllerScript : MonoBehaviour
         {
             vertical = 0.0f;
         }
-        Vector2 position = rb.position;
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Debug.Log("Left Shift key was pressed");
-        }
-        
+
+        // if shift key is pressed
         if(Input.GetKey(KeyCode.LeftShift))
         {
-            position.x += Time.deltaTime * runningSpeed * horizontal;
-            position.y += Time.deltaTime * runningSpeed * vertical;
-        } else {
-            position.x += Time.deltaTime * walkingSpeed * horizontal;
-            position.y += Time.deltaTime * walkingSpeed * vertical;
+            speed = runningSpeed;
+        } else
+        {
+            speed = walkingSpeed;
         }
+
+        // if stamina is enabled
+        if(enableStamina)
+        {
+            // update once a second
+            if(Time.time >= staminaTimeout)
+            {
+                staminaTimeout = Mathf.FloorToInt(Time.time) + staminaSpeed;
+
+                // if running
+                if(speed > walkingSpeed) 
+                {
+                    // if running out of stamina
+                    if(stamina < staminaLoseMultiplier)
+                    {
+                        // forced to walk
+                        speed = walkingSpeed;
+                        stamina = Mathf.Clamp(stamina + staminaGainMultiplier, 0, maxStamina);
+                        Debug.Log("walking and gaining");
+                        staminaDisabled = true;
+                    }
+                    else 
+                    {
+                        if(staminaDisabled && stamina < Mathf.Round(maxStamina * 0.4f))
+                        {
+                            speed = walkingSpeed;
+                            stamina = Mathf.Clamp(stamina + staminaGainMultiplier, 0, maxStamina);
+                            Debug.Log("Stamina disabled");
+                        }
+                        else
+                        {
+                            // run and lose stamina
+                            stamina = Mathf.Clamp(stamina - staminaLoseMultiplier, 0, maxStamina);
+                            Debug.Log("running and losing");
+                            staminaDisabled = false;
+                        }
+                    }
+                }
+                else
+                {
+                    // walking voluntarily and gaining
+                    Debug.Log("walking voluntarily and gaining");
+                    stamina = Mathf.Clamp(stamina + staminaGainMultiplier, 0, maxStamina);
+                }
+                Debug.Log("Stamina: " + stamina + "/" + maxStamina + ". Speed: " + speed + "/" + runningSpeed);
+            }
+        }
+        
+        Vector2 position = rb.position;
+        position.x += Time.deltaTime * speed * horizontal;
+        position.y += Time.deltaTime * speed * vertical;
         
         rb.MovePosition(position);
     }
