054b4bcd0f7b9e882929ceab203475ac090742b6
Hamza Sar <hamoz.sq78@gmail.com>
2020-08-30 20:59:48 -0400

    UI is now automatically visible in the Game View

---
+++ Assets/Scenes/Scripts/CanvasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool autoEnableAlpha = true;
+    void Start()
+    {
+        if(autoEnableAlpha)
+        {
+            CanvasGroup cg = GetComponent<CanvasGroup>();
+            cg.alpha = 1.0f; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
