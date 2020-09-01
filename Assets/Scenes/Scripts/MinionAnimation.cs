86a3c14bd1c693067637214ea1dd5fcbd5b1ad59
cheezytaco101 <marcuskuffner3125@gmail.com>
2020-08-31 20:31:12 -0400

    Added new tiles, added animations to bob, and new target changing script '

---
+++ Assets/Scenes/Scripts/MinionAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAnimation : MonoBehaviour
+{
+    float horizontal =  GameObject.FindGameObjectWithTag("Minion").transform.position.x, 
+    vertical =  GameObject.FindGameObjectWithTag("Minion").transform.position.y, 
+    prevHorizontal, prevVertical, deltaHorizontal, deltaVertical;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prevHorizontal = horizontal;
+        prevVertical = vertical;
+        horizontal =  GameObject.FindGameObjectWithTag("Minion").transform.position.x;
+        vertical =  GameObject.FindGameObjectWithTag("Minion").transform.position.y;
+        deltaVertical = vertical - prevVertical;
+        deltaHorizontal = horizontal - prevHorizontal;
+        if(deltaVertical == 0 && deltaHorizontal == 0)
+        {
+            animator.SetBool("Walking", false);
+        } else {
+            animator.SetBool("Walking", true);
+        }
+        animator.SetFloat("MoveX", deltaHorizontal);
+        animator.SetFloat("MoveY", deltaVertical);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scenes/Scripts/MinionTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MinionTarget : MonoBehaviour
+{
+    AIDestinationSetter destination;
+    public GameObject house;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = GetComponent<AIDestinationSetter>();
+        destination.target = transform.Find(house.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
